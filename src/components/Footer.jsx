4894b40cf72605eefbb5fb5fae4c58f47ae9614f
NarleeBro <linarpr@gmail.com>
2023-07-23 19:39:32 +0300

    time footer auto

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,17 @@
 export default function Footer() {
+  
+  return (
+    <footer className="footer section">
+      <p className="footer__copyright">©&nbsp;{new Date().getFullYear()} Mesto Russia</p>
+    </footer>
+  );
+}
+
+
+/* export default function Footer() {
   return (
     <footer className="footer section">
       <p className="footer__copyright">©&nbsp;2020 Mesto Russia</p>
     </footer>
   );
-}
+} */
